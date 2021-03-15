18ef351a8fcd74f034a2c50c88a10b0d068d58f5
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2021-03-15 13:25:40 +0600

    Update leave_management.vue

--- resources/js/components/admin/leave_management/leave_management.vue
+++ resources/js/components/admin/leave_management/leave_management.vue
@@ -1,70 +1,72 @@
 <template>
   <div>
     <component v-bind:is="'navbar'"></component>
-    <div class="d-flex justify-content-evenly">
-      <div class="bg-info fifty">
-        <div v-if="showAddNewPolicy">
-          <form
-            class="d-flex justify-content-between"
-            @submit.prevent="addNewPolicy"
-          >
-            <input type="text" v-model="newPolicyName" />
-            <button type="submit">Save</button>
-          </form>
-        </div>
-        <div class="d-flex justify-content-between">
-          <div>Policies</div>
-          <div>
-            <button @click="addNewPolicy">Add new</button>
+    <div class="container mt-5">
+      <div class="d-flex justify-content-evenly">
+        <div class="bg-info fifty">
+          <div v-if="showAddNewPolicy">
+            <form
+              class="d-flex justify-content-between"
+              @submit.prevent="addNewPolicy"
+            >
+              <input type="text" v-model="newPolicyName" />
+              <button type="submit">Save</button>
+            </form>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>Policies</div>
+            <div>
+              <button @click="addNewPolicy">Add new</button>
+            </div>
+          </div>
+          <div v-for="(policy, i) in allPolicies" :key="i">
+            <form class="d-flex justify-content-between">
+              <button @click.prevent="renderLeaves(policy)">
+                {{ policy.name }}
+              </button>
+              <div>
+                <button>Edit</button>
+              </div>
+            </form>
           </div>
         </div>
-        <div v-for="(policy, i) in allPolicies" :key="i">
-          <form class="d-flex justify-content-between">
-            <button @click.prevent="renderLeaves(policy)">
-              {{ policy.name }}
-            </button>
-            <div>
-              <button>Edit</button>
-            </div>
-          </form>
-        </div>
-      </div>
 
-      <div class="bg-danger fifty">
-        <div v-if="showAddNewLeave">
-          <form
-            class="d-flex justify-content-between"
-            @submit.prevent="insertNewLeave"
-          >
-            <label for="name">Name</label>
-            <input id="name" type="text" v-model="newLeaveName" />
-            <label for="payment_type">payment_type</label>
-            <input
-              id="payment_type"
-              type="text"
-              v-model="newLeavePaymentType"
-            />
-            <label for="days">days</label>
-            <input id="days" type="text" v-model="newLeaveDays" />
-            <button type="submit">Save</button>
-          </form>
-        </div>
-
-        <div class="d-flex justify-content-between">
-          <div>Leaves: {{ policy.name ? policy.name : "" }}</div>
-          <div>
-            <button @click.prevent="addNewLeave">Add new</button>
+        <div class="bg-danger fifty">
+          <div v-if="showAddNewLeave">
+            <form
+              class="d-flex justify-content-between"
+              @submit.prevent="insertNewLeave"
+            >
+              <label for="name">Name</label>
+              <input id="name" type="text" v-model="newLeaveName" />
+              <label for="payment_type">payment_type</label>
+              <input
+                id="payment_type"
+                type="text"
+                v-model="newLeavePaymentType"
+              />
+              <label for="days">days</label>
+              <input id="days" type="text" v-model="newLeaveDays" />
+              <button type="submit">Save</button>
+            </form>
           </div>
-        </div>
-        <div v-for="(leave, i) in allLeaves" :key="i">
-          <form class="d-flex justify-content-between">
+
+          <div class="d-flex justify-content-between">
+            <div>Leaves: {{ policy.name ? policy.name : "" }}</div>
             <div>
-              {{ leave }}
+              <button @click.prevent="addNewLeave">Add new</button>
             </div>
-            <div>
-              <button>Edit</button>
-            </div>
-          </form>
+          </div>
+          <div v-for="(leave, i) in allLeaves" :key="i">
+            <form class="d-flex justify-content-between">
+              <div>
+                {{ leave }}
+              </div>
+              <div>
+                <button>Edit</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
@@ -87,7 +89,7 @@ export default {
     };
   },
   async mounted() {
-    const response = await axios.get("http://localhost:8000/api/policies");
+    const response = await axios.get("/api/policies");
     console.log(response.data);
     this.allPolicies = response.data;
   },
@@ -98,14 +100,15 @@ export default {
       // const response = await axios.post("");
     },
     async getAllPolicies() {
-      const response = await axios.get("http://localhost:8000/api/policies");
+      const response = await axios.get("/api/policies");
       const data = response.data;
       console.log(data);
       this.allPolicies = data;
       console.log(this.allPolicies);
     },
     async addNewPolicy() {
-      const response = await axios.post("http://localhost:8000/api/policies", {
+      console.log("Policy called");
+      const response = await axios.post("/api/policies", {
         name: this.newPolicyName,
       });
       console.log(response.data);
@@ -119,7 +122,7 @@ export default {
       console.log(this.newLeaveName);
       console.log(this.newLeavePaymentType);
       console.log(this.newLeaveDays);
-      const response = await axios.post("http://localhost:8000/api/leaves", {
+      const response = await axios.post("/api/leaves", {
         name: this.newLeaveName,
         payment_type: this.newLeavePaymentType,
         days: this.newLeaveDays,
