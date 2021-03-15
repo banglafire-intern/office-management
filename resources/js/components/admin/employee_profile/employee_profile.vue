807d66807c952b0f9b6263948c152d32c1fb59fd
Md.Muhtasim Fuad <fuadnafiz98@gmail.com>
2021-03-15 14:43:16 +0600

    some modifications

--- resources/js/components/admin/employee_profile/employee_profile.vue
+++ resources/js/components/admin/employee_profile/employee_profile.vue
@@ -1,16 +1,107 @@
 <template>
-  <div>
-    <component v-bind:is="'navbar'"></component>
-    Employee
-  </div>
+    <div class="container p-3">
+        <div class="row justify-content-lg-center">
+            <div class="col col-xs-1">Username</div>
+            <div class="col-xxl-auto">
+                <div v-if="toggle">
+                    <select
+                        name="LeaveType"
+                        @change="onChange($event)"
+                        class="form-control form-control-md"
+                        v-model="key"
+                    >
+                        <!-- <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Policies
+            </button> -->
+                        <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"> -->
+                        <option value="">Select Policies</option>
+                        <option class="dropdown-item" value="1st Year"
+                            >1st year</option
+                        >
+                        <option class="dropdown-item" value="2"
+                            >2nd year</option
+                        >
+                        <!-- <option class="dropdown-item">Junior Developer</option>
+              <option class="dropdown-item">Senior Developer</option> -->
+                        <!-- </div> -->
+                        <!-- </select>
+            <option value="1">Annual Leave/ Off-Day</option>
+            <option value="2">On Demand Leave</option>
+             -->
+                    </select>
+                </div>
+                <div v-else>
+                    <p>Policy</p>
+                </div>
+            </div>
+
+            <div class="col col-xs-1">
+                <div v-if="toggle">
+                    <button
+                        v-on:click="handleSubmit"
+                        type="button"
+                        class="btn btn-outline-dark btn-sm rounded mx-3"
+                    >
+                        Save
+                    </button>
+                    <button
+                        v-on:click="toggle = !toggle"
+                        type="button"
+                        class="btn btn-outline-dark btn-sm rounded mx-3"
+                    >
+                        Cancel
+                    </button>
+                </div>
+                <div v-else>
+                    <button
+                        v-on:click="toggle = !toggle"
+                        type="button"
+                        class="btn btn-outline-dark btn-sm rounded mx-3"
+                    >
+                        Edit
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import { createPopper } from "@popperjs/core";
 export default {
-
-}
+    name: "AssignedPolicy",
+    props: {},
+    data() {
+        return {
+            toggle: false
+        };
+    },
+    components: {},
+    mounted() {
+        const button = document.querySelector("#drop");
+        // const tooltip = document.querySelector('#tooltip');
+        createPopper(button);
+    },
+    methods: {
+        toggleAdd() {
+            this.showAdd = !this.showAdd;
+        },
+        onChange(event) {
+            console.log(event.target.value);
+        },
+        handleSubmit: function() {
+            // console.log(this.$refs.item.value)
+            this.toggle = !this.toggle;
+            this.title = this.$refs.item.value;
+        }
+    }
+};
 </script>
-
-<style>
-
-</style>
+<style scoped></style>
